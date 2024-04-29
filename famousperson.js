c36f98327e751fd76011c49b3128dce108125a20
muhammadfaizan4263 <160520200+muhammadfaizan4263@users.noreply.github.com>
2024-04-29 23:44:33 +0500

    Add files via upload

---
+++ famousperson.js
@@ -0,0 +1,3 @@
+var famousperson = "Albert elistein";
+var message = "one said, ''A person who nerer made a mistake never the";
+console.log(famousperson, message);
---
+++ famousperson.ts
@@ -0,0 +1,6 @@
+let famousperson = "Albert elistein";
+
+let message = "one said, ''A person who nerer made a mistake never the";
+
+console.log(famousperson, message);
+
