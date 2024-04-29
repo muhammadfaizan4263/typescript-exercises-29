330500b93921c9cf42936f7020234822a96bd4fa
muhammadfaizan4263 <160520200+muhammadfaizan4263@users.noreply.github.com>
2024-04-29 23:43:58 +0500

    Add files via upload

---
+++ WellcomeAI.js
@@ -0,0 +1,5 @@
+console.log("hello, world");
+console.log("Wellcome to A.I world");
+var myName = 'Sir Zia';
+var message = 'is a good person';
+console.log(myName + " " + message);
---
+++ personName.ts
@@ -0,0 +1,7 @@
+let personname : string = "faizan MUGHAL";
+
+console.log(personname.toLowerCase());
+
+console.log(personname.toUpperCase());
+
+console.log(personname.replace(/\b\w/g, (char) => char.toUpperCase()));
